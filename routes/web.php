b13f97a61de27c376829f9128fface4fab7f8294
aurelpa <arelggsavage780@gmail.com>
2026-03-04 13:52:25 +0700

    kawan

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
- 
-
 Route::get('/', function () {
-
     return view('welcome');
-
 });
 
- 
+// Route teman pertama
+Route::get('/teman', function () {
+    return '<h1>Halo! Nama saya Adam</h1>';
+});
 
-// Route baru – wajib ditambahkan!
-
-Route::get('/perkenalan', function () {
-
-    return '<h1>Halo! Nama saya [Adam Andika Sukma]</h1>
-
-            <p>NIM: [4124001] | Prodi: Sistem Informasi</p>
-
+// Route teman kedua (Arel)
+Route::get('/perkenalan-arel', function (): string {
+    return '<h1>Halo! Nama saya Arel</h1>
+            <p>NIM: 4124027</p>
+            <p>Prodi: Sistem Informasi</p>
             <p>Saya siap belajar Laravel! 🚀</p>';
-
 });
