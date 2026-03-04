36865e1d098a131d62e14a115613da95d987b64f
aurelpa <arelggsavage780@gmail.com>
2026-03-04 13:58:59 +0700

    teman

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::get('/', function () {
 // Route teman pertama
 Route::get('/teman', function () {
     return '<h1>Halo! Nama saya Adam</h1>';
+     return '<h1>Halo! Nama saya Arel</h1>
+            <p>NIM: 4124027</p>
+            <p>Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
 });
 
 // Route teman kedua (Arel)
