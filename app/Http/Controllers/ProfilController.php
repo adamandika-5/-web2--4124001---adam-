f8a803ba2c557f3b60f983ccd7e2493594f16f17
adamandika-5 <andikaadam91@gmail.com>
2026-03-13 23:39:18 +0700

    edit

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -8,8 +8,8 @@ class ProfilController extends Controller
     public function index()
     {
         $data = [
-            'nama' => 'Aurel Prayoga Arandy',
-            'nim' => '4124027',
+            'nama' => 'Adam Andika Sukma',
+            'nim' => '4124001',
             'prodi' => 'Sisitem Informasi',
             'semester' => 4,
             'keahlian' => ['Laravel', 'HTML', 'CSS', 'JavaScript']
